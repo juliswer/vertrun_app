30a70076f8a0e96c7043ccac6291cb46c551374a
Julian Swerdlin <julianswer36@gmail.com>
2022-03-07 15:39:30 -0300

    making my own info

--- src/data.js
+++ src/data.js
@@ -4,37 +4,6 @@ export const data = {
     image: 'https://img.a.transfermarkt.technology/portrait/big/28003-1631171950.jpg?lm=1',
     age: "17",
     city: "Buenos Aires, Argentina",
-    activities: [
-        {
-            id: 1,
-            name: 'Running',
-            date: '2020-01-01',
-        }
-        /* "running": {
-            name: "running",
-            date: "2022-06-13",
-            distance: "10km",
-            time: "1h 30min",
-            elevationGain: "1700cal",
-            id: "1"
-        },
-        "cycling": {
-            name: "cycling",
-            date: "2022-02-20",
-            distance: "20km",
-            time: "2h 30min",
-            elevationGain: "1400cal",
-            id: "2"
-        },
-        "swimming": {
-            name: "swimming",
-            date: "2022-01-10",
-            distance: "5km",
-            time: "30min",
-            elevationGain: "1200cal",
-            id: "3"
-        } */
-    ]
 }
 
 export const activities = [
@@ -50,9 +19,19 @@ export const activities = [
     {
         id: 2,
         name: "Cycling",
+        image: 'https://cdn.mos.cms.futurecdn.net/foejuANBeiCtYkT6PaXW9F-768-80.jpg',
         date: "2022-02-20",
         distance: "20km",
         time: "2h 30min",
         elevationGain: "1400cal",
+    },
+    {
+        id: 3,
+        name: "Swimming",
+        image: 'https://salfordcommunityleisure.co.uk/wp-content/uploads/2021/06/Swimming-WEB.png',
+        date: "2022-01-10",
+        distance: "5km",
+        time: "30min",
+        elevationGain: "1200cal",
     }
 ]
