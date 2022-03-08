543b684c7f5447dc7670dd6825ea185386a31dc5
Julian Swerdlin <julianswer36@gmail.com>
2022-03-08 14:41:55 -0300

    stats page

--- src/pages/stats.jsx
+++ src/pages/stats.jsx
@@ -9,11 +9,13 @@ const Stats = ({user}) => {
   return (
     <div className="container mx-auto">
       <h1 className="text-3xl font-bold my-5 text-white">These are the stats from <a target="_blank" className="underline text-rose-500" rel="noreferrer" href={userInfo.id !== '' ? `https://strava.com/athletes/${userInfo.id}` : 'https://www.strava.com/athletes/99855324'}>{user.name} {user.lastname}</a> (Last Three Months) 📅</h1>
+      <div className="grid grid-cols-3">
       {lastMonths.map((month) => (
-        <div className="card" key={month.month}>
-          <h2 className="text-white">{month.month}</h2>
+        <div className="hover:scale-110 ease-in duration-200 mx-2 block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700" key={month.month}>
+          <h2 className="text-white text-2xl font-bold">{month.month}</h2>
         </div>
       ))}
+      </div>
     </div>
   )
 }
