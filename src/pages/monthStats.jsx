611c5787b53d873ebb7e07a55d9a4719668e1dd1
Julian Swerdlin <julianswer36@gmail.com>
2022-03-08 16:41:07 -0300

    stats page

--- src/pages/monthStats.jsx
+++ src/pages/monthStats.jsx
@@ -43,7 +43,7 @@ const MonthStats = ({user}) => {
                     <img src={activity.image} className="rounded-lg shadow-lg mb-4" alt="" />
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{activity.name}</h5>
                     <p className="font-normal text-gray-700 dark:text-gray-400">{activity.distance ? `Distance Done: ${activity.distance}` : ''}</p>
-                    <p className="font-normal text-gray-700 dark:text-gray-400">Last Done: {activity.date}</p>
+                    <p className="font-normal text-lime-200">Effectivity: {activity.effectivity}</p>
                     <p className="font-normal text-rose-300">Duration: {activity.time}</p>
                     <p className="font-normal text-lime-500">{activity.elevationGain}</p>
                     </a>
